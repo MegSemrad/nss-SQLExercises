82779bf1ffbdad4bc057a2025c603eca411b6cb3
Meghan Semrad <mlsemrad753@gmail.com>
2021-03-10 13:14:56 -0600

    save

--- SQLExercises.sql
+++ SQLExercises.sql
@@ -1,19 +1,27 @@
--- Doo not want to run all of code everytime hit the run button. To avoid this, select whatever it is 
+-- Do not want to run all of code everytime hit the run button. To avoid this, select whatever it is 
 -- that needs to be run and then press button. It is perfectly fine to leave old code. Can also comment 
 -- out code as go along. 
+
+
 
 
 INSERT INTO Genre (Label) VALUES ('Techno');
 
 
-select SongLength from Song where Id = 18;
 
 
-update Song
-set SongLength = 515
-where Id = 18;
+SELECT SongLength FROM Song WHERE Id = 18;
+
+
+
+
+UPDATE Song
+SET SongLength = 515
+WHERE Id = 18;
 -- Once you run the update statement, in order to make sure the value has changed, we run the select query again.
-select SongLength from Song where Id = 18;
+SELECT SongLength FROM Song WHERE Id = 18;
+
+
 
 
 INSERT INTO Song (Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId) VALUES ('Good Day Sunshine', 350, '8/5/1966', 7, 6, 5);
@@ -25,11 +33,14 @@ INSERT INTO Song (Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId) VA
 
 
 
+
 -- 1. Query all of the entries in the Genre table
         SELECT
             Id,
             Label
         FROM Genre;
+
+
 
 
 -- 2. Query all the entries in the Artist table and order by the artist's name. HINT: use the ORDER BY keywords
@@ -41,11 +52,15 @@ INSERT INTO Song (Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId) VA
             ORDER BY a.ArtistName;
 
 
+
+
 -- 3. Write a SELECT query that lists all the songs in the Song table and include the Artist name
         SELECT s.Title,
             a.ArtistName
         FROM Song s
-            LEFT JOIN Artist a on s.ArtistId = a.id;
+            LEFT JOIN Artist a ON s.ArtistId = a.id;
+
+
 
 
 -- 4. Write a SELECT query that lists all the Artists that have a Pop Album
@@ -53,23 +68,130 @@ INSERT INTO Song (Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId) VA
             Artist.ArtistName, 
             Genre.Label
         FROM Album
-            LEFT JOIN Artist on Album.ArtistId = Artist.Id
-            LEFT JOIN Genre on Album.GenreId = Genre.Id
+            LEFT JOIN Artist ON Album.ArtistId = Artist.Id
+            LEFT JOIN Genre ON Album.GenreId = Genre.Id
         WHERE 
             GenreId = 7; -- there will be no results as no artist has a pop album
 
 
 
+
 -- 5. Write a SELECT query that lists all the Artists that have a Jazz or Rock Album
+        SELECT 
+            Artist.ArtistName, 
+            Genre.Label
+        FROM Album
+            LEFT JOIN Artist ON Album.ArtistId = Artist.Id
+            LEFT JOIN Genre ON Album.GenreId = Genre.Id
+        WHERE 
+            GenreId = 2 OR GenreId = 4;
+
+
 
 
 -- 6. Write a SELECT statement that lists the Albums with no songs
 
 
+
+
 -- 7. Using the INSERT statement, add one of your favorite artists to the Artist table.
+        INSERT INTO Artist(ArtistName, YearEstablished) VALUES ('Adele', 2007)
+
+
 
 
 -- 8. Using the INSERT statement, add one, or more, albums by your artist to the Album table.
+        INSERT INTO Album(Title, ReleaseDate, AlbumLength, Label, ArtistId, GenreId) VALUES ('19', '01/28/2008', 4341, 'XL Recordings', 29, 1)
+
+
 
 
 -- 9. Using the INSERT statement, add some songs that are on that album to the Song table.
+
+        INSERT INTO Song(Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId) VALUES ('DayDreamer', 341, '01/28/2008', 1, 29, 27)
+        INSERT INTO Song(Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId) VALUES ('Chasing Pavements', 331, '01/28/2008', 1, 29, 27)
+        INSERT INTO Song(Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId) VALUES ('Best For Last', 419, '01/28/2008', 1, 29, 27)
+
+
+-- 10. Write a SELECT query that provides the song titles, album title, and artist name for all of the data you just entered 
+--     in. Use the LEFT JOIN keyword sequence to connect the tables, and the WHERE keyword to filter the results to the album 
+--     and artist you added.
+-- THIS EXAMPLE DONE IN CLASS   
+        Select *
+            FROM Song s 
+                LEFT JOIN Album al ON s.AlbumId = al.Id
+                LEFT JOIN Artist ar ON s.ArtistId = ar.Id
+            WHERE s.Title = 'TV Party' 
+                OR s.Title = 'Thug' 
+                OR ar.ArtistName = 'Beatles'
+
+
+        -- this will give back  names/titles only and not the additional information attatched to those tables         
+        Select s.Title, al.Title, ar.ArtistName
+            FROM Song s 
+                LEFT JOIN Album al ON s.AlbumId = al.Id
+                LEFT JOIN Artist ar ON s.ArtistId = ar.Id
+            WHERE s.Title = 'TV Party' 
+                OR s.Title = 'Thug' 
+                OR ar.ArtistName = 'Beatles'
+        
+
+        -- can create alias as some column titles might be ambiguous (previously both song and album title columns were titled "Title")         
+        Select s.Title AS SongTitle, al.Title AS AlbumTitle, ar.ArtistName
+            FROM Song s 
+                LEFT JOIN Album al ON s.AlbumId = al.Id
+                LEFT JOIN Artist ar ON s.ArtistId = ar.Id
+            WHERE s.Title = 'TV Party' 
+                OR s.Title = 'Thug' 
+                OR ar.ArtistName = 'Beatles'
+
+
+
+-- 11. Write a SELECT statement to display how many songs exist for each album. You'll need to use the COUNT() function and the GROUP BY keyword sequence.
+-- THIS EXAMPLE DONE IN CLASS 
+
+        -- aggregate funcation is make single value out of many (?)
+        SELECT count(s.id) AS SongCount, al.Title           -- and then count the number of dong ids within each group 
+            FROM song s 
+                LEFT JOIN  Album al ON s.AlbumId = al.Id    -- will be getting information from both song and album 
+            GROUP BY al.Title                               -- group by album title 
+
+
+        -- Write this way if wish to add artist's name associate with each album
+        SELECT count(s.id) AS SongCount, al.Title as AlbumTitle, ar.ArtistName 
+            FROM song s 
+                LEFT JOIN  Album al ON s.AlbumId = al.Id
+                LEFT JOIN Artist ar ON al.ArtistId = ar.Id
+            GROUP BY al.Title, ar.ArtistName 
+
+
+
+
+
+-- 12. Write a SELECT statement to display how many songs exist for each artist. You'll need to use the COUNT() function and the GROUP BY keyword sequence.
+
+here is a change...will it save?
+
+
+-- 13. Write a SELECT statement to display how many songs exist for each genre. You'll need to use the COUNT() function and the GROUP BY keyword sequence.
+
+
+
+
+-- 14. Write a SELECT query that lists the Artists that have put out records on more than one record label. Hint: When using GROUP BY instead of using a WHERE 
+--     clause, use the HAVING keyword
+
+
+
+
+-- 15. Using MAX() function, write a select statement to find the album with the longest duration. The result should display the album title and the duration.
+
+
+
+
+-- 16. Using MAX() function, write a select statement to find the song with the longest duration. The result should display the song title and the duration.
+
+
+
+
+-- 17. Modify the previous query to also display the title of the album.
